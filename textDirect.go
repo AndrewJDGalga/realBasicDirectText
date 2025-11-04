1daa673a3dfdea102641e90ca67a1aa24acfd007
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-11-04 15:45:12 -0600

    play with flag for setting buffersize, printing flag to use it and avoid go error.

--- textDirect.go
+++ textDirect.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
-	"time"
+	"os"
 )
 
 func main() {
-	go listeningForMsg()
-	time.Sleep(2 * time.Second)
-	sendMsg()
+	var sizeFlag = flag.Int("s", 1024, "Size of message receipt buffer. Defaults to 1024.")
+	progMode := len(os.Args)
+
+	fmt.Println(progMode)
+	fmt.Println(*sizeFlag)
+	/*
+		go listeningForMsg()
+		time.Sleep(2 * time.Second)
+		sendMsg()
+	*/
 }
 
-func sendMsg() {
+func sendMsg(addr string, msg string) {
 	fmt.Println("Transmitting...")
 	conn, err := net.Dial("tcp", "127.0.0.1:8080")
 	if err != nil {
@@ -24,9 +32,10 @@ func sendMsg() {
 	fmt.Println("...Complete.")
 }
 
-func listeningForMsg() {
+func listeningForMsg(bufferSize int) {
 	ln, err := net.Listen("tcp", ":8080")
 	fmt.Println("Listening on ", ln.Addr().String())
+	fmt.Println("Press Ctrl+C to Quit.")
 	if err != nil {
 		fmt.Println("Dial error: ", err)
 		return //empty return seems standard but unclear on result in goroutine
@@ -38,13 +47,15 @@ func listeningForMsg() {
 			break
 		}
 		if conn != nil {
-			tmp := make([]byte, 1024)
+			tmp := make([]byte, bufferSize)
 			size, err := conn.Read(tmp)
 			if err != nil && size > 0 {
 				fmt.Println(err)
 				return //if empty return is valid, then ok here?
 			}
 			fmt.Println("Received: ", string(tmp[:size]))
+
+			//testing
 			conn.Close()
 			ln.Close()
 			return

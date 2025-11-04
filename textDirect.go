715f1020c6133552ef561f901e94e761894c4857
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-11-04 00:24:05 -0600

    basic transmission test from godocs (might be bad commit before this)

--- textDirect.go
+++ textDirect.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net"
+)
 
+func main() {
+	fmt.Println("Transmitting...")
+	conn, err := net.Dial("tcp", "127.0.0.1:8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Fprintf(conn, "Hello World")
+	conn.Close()
+	fmt.Println("...Complete.")
 }
